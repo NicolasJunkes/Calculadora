35ecc810e48bcc5f723a0c685b5d18607b1034ce
Nicolas Jukes <nicolas.junkes12@outlook.com>
2019-05-17 15:40:13 -0300

    Calculadora simples

---
+++ ProjetosLogicos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetosLogicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, resultado;
+            char operador;
+
+            Console.WriteLine("Infome o um número");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe o operador desejado");
+            operador = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Informe um segundo número");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (operador == '+')
+            {
+                resultado = num1 + num2;
+                Console.WriteLine(resultado);
+            }
+            else if (operador == '-')
+            {
+                resultado = num1 - num2;
+                Console.WriteLine(resultado);
+            }
+            else if (operador == '/')
+            {
+                resultado = num1 / num2;
+                Console.WriteLine(resultado);
+            }
+            else
+            {
+                resultado = num1 * num2;
+                Console.WriteLine(resultado);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
